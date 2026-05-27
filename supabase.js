84f7e2b2e34d0bb85cbeb9ebb90a0d89c8c2ccda
MatteoCambielli <116428032+MatteoCambielli@users.noreply.github.com>
2026-05-27 11:37:20 +0200

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,6 +1,6 @@
-// Configurazione di Supabase (Pronto per essere collegato)
-const SUPABASE_URL = 'https://YOUR_SUPABASE_PROJECT_URL.supabase.co';
-const SUPABASE_KEY = 'YOUR_SUPABASE_ANON_KEY';
+// Configurazione di Supabase
+const SUPABASE_URL = 'https://xurlmqpdzgspvihridva.supabase.co';
+const SUPABASE_KEY = 'sb_publishable_1SIFclDhSwnsISlEEvjIgQ_QGH3BYkx';
 
 // Inizializzazione del client globale
 const supabaseClient = window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
